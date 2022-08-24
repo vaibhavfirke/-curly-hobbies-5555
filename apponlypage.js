3cc4a0f8f903bfe1c1ef03a352922a2ab55235d1
piyush2205 <pk.rv.668@gmail.com>
2022-08-24 23:20:57 +0530

    apponlypage-piyush

---
+++ apponlypage.js
@@ -0,0 +1,152 @@
+productData=[
+    {
+    title: "Original Xiaomi MIJIA K Song Karaoke Microphone Bluetooth 5.1 Stereo Sound DSP Chip Noise Cancellation 2500mAh Handheld Microphones",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "1",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16456/goods_thumb_220-v3/db73a29c5068.jpg",
+  },
+  {
+    title: "Original Xiaomi Mi Super Durable Writing Sign Pen 0.5mm Bullet Black Red Colorful Ink Study Office Gel Pen",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "2",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16461/goods_thumb_220-v1/9d84d26108a2.jpg",
+  }, 
+    {
+    title: "Replacement Toothbrush Head Accessories for SOOCARE SOOCAS V1 V2 X1 X3 X3U X5 Electric Toothbrush",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "3",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16422/goods_thumb_220-v2/67fedd102dee.jpg",
+  },  
+   {
+    title: "Xiaomi Aqara Smart Wall Switch ZigBee Version Single Fire / Zero Fire Line Light Wireless Apple Homekit Mi Home APP Control",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "4",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16488/goods_thumb_220-v1/1efea93ac4a5.jpg",
+  }, 
+    {
+    title: "Bluetooth Sleeping Headphones Sports Headband Thin Soft Elastic Comfortable Wireless Music Earphones Eye Mask for Side Sleeper",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "5",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6771730315905003520/16391/goods_thumb_220-v1/0fc8747c9e18.jpg",
+  },  
+   {
+    title: "Original Xiaomi Mi Airdots Air 2 SE TWS Bluetooth Earphone Wireless Headphone Dual SBC/AAC Tap Stereo Control Dual MIC",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "6",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16463/goods_thumb_220-v7/2f380d04f8f3.jpg",
+  },  
+   {
+    title: "Xiaomi Redmi Note 10S Global Version Smartphone 64MP Camera Helio G95 5000mAh 33W AMOLED DotDisplay",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "7",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6771730315905003520/16393/goods_thumb_220-v1/9859bd33f57c.jpg",
+  }, 
+    {
+    title: "Xiaomi DUKA RS1 24 in1 / RS2 33 in 1 Multi-purpose Ratchet Wrench Screwdriver S2 Magnetic Bits Tools Set DIY Household Repair Tool",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "8",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16458/goods_thumb_220-v1/5ff3325fa799.jpg",
+  },  
+   {
+    title: "Y68 SSmart Watch Men Smartwatch For WomenMen Watches Kids Children Fitness Bracelet Smart Watch For Women Smartwatch",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "9",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878026654363611136/16419/goods_thumb_220-v1/316534621fbd.jpg",
+  },  
+   {
+    title: "Xiaomi Honeywell Fire Alarm Smoke Sensor Gas Detector Work With Xiaomi Gateway 2 Smart Home Security Mijia APP Control",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "10",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16417/goods_thumb_220-v1/af9e85ce05a7.jpg",
+  },   
+  {
+    title: "New Global Version Cell Mobile Phones p50 pro 16G+768GB Daul Sim 5G Celulares Cameras Telefon 6800mAh Battery Smart phone",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "11",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16473/goods_thumb_220-v2/5ea84e7d790c.jpg",
+  },  
+   {
+    title: "Xiaomi Mijia Hand Washer Pro Automatic Induction Foaming Soap Dispenser 20s Rubbing Bubble Reminder Smart Home Life for Children",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "12",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16477/goods_thumb_220-v1/fe4c93a2b74a.jpg",
+  },  
+   {
+    title: "Massage Gun Deep Tissue Percussion Mini LCD 32 Speeds Muscle Massager Pain Relief Body Relax Fitness",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "13",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6878024886355423232/16453/goods_thumb_220-v1/8ab1a920d820.jpg",
+  }, 
+    {
+    title: "Global Version MI 12 Ultra 7.3 inch Smartphone Cell Phone Unlocked 2022 5G Mobile Phones Cheap Android M12 Telephone",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "14",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16485/goods_thumb_220-v1/3dd91decfe1a.jpg",
+  },  
+   {
+    title: "New M4PRO 6.7 Inch Global Version 256GB/512GB 6800mAh 16MP+32MP Smartphone Fingerprint Face ID Deca Core Mobile Phones MTK6889",
+    price: "$69.92",
+    orignalprice:"$69.99",
+    productID: "15",
+    img: "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16563/goods_thumb_220-v1/e254e052fe64.jpg",
+  },
+]
+
+
+
+
+
+let append_data=()=>{
+    let contanier=document.getElementById("results")
+
+    productData.forEach(el => {
+        let div =document.createElement("div")
+        div.setAttribute("class","psdiv")
+        let imgdiv=document.createElement("div")
+        imgdiv.setAttribute("class","psdivimg")
+
+        let img=document.createElement("img")
+        img.src=el.img
+
+
+        let ptitle=document.createElement("p")
+        ptitle.innerText=el.title
+        ptitle.style.cursor="pointer"
+
+        
+
+
+        let pprice=document.createElement("p")
+        pprice.innerText=el.price
+        pprice.style.color="red"
+        pprice.style.fontSize="20px"
+
+        let porignalprice=document.createElement("p")
+        porignalprice.innerText=el.orignalprice
+        porignalprice.style.color="#aab1ad"
+
+        imgdiv.append(img)
+        
+        div.append(imgdiv,ptitle,pprice,porignalprice)
+
+        contanier.append(div)
+
+
+    });
+}
+
+append_data()
