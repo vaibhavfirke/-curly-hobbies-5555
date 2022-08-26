2cc7651f24780c87e2f793a7b1e185462b29d924
piyush2205 <pk.rv.668@gmail.com>
2022-08-26 23:44:10 +0530

    newarrivalday3-piyush

---
+++ newarrivals.js
@@ -0,0 +1,362 @@
+Arrayofanimataion=[
+    {
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        RRP:240.00,
+      img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+
+    },
+    {
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        RRP:240.00,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+      },
+    
+      { price: 278.00,
+        RRP:240.00,
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+      },
+    
+      { price: 278.00,
+        RRP:240.00,
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+      },
+    
+    //   { price: 278.00,
+    //     RRP:240.00,
+    //     title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+    //     img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+    //   },
+    //   {
+    //     title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+    //     price: 278.00,
+    //     RRP:240.00,
+    //     img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+    //   },
+    //   {title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+    //     img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+    //   },
+    //   {
+    //     title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+    //     price: 278.00,
+    //     RRP:240.00,
+    //     img:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16595/goods_thumb_220-v1/0851ce5b0b82.jpg" ,
+  
+    //   },
+          
+
+]
+
+psnewarrivaldata=[
+    {
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },
+    {
+        title: "Original Xiaomi Mi Airdots Air 2 SE TWS Bluetooth Earphone Wireless Headphone Dual SBC/AAC Tap Stereo Control Dual MIC",
+        price: 24.88,
+
+        productID: 2,
+        img: "https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16463/goods_thumb_220-v7/2f380d04f8f3.jpg",
+      },  
+       {
+        title: "Xiaomi Redmi Note 10S Global Version Smartphone 64MP Camera Helio G95 5000mAh 33W AMOLED DotDisplay",
+        price: 529.19,
+      
+        productID: 3,
+        img: "https://gloimg.gbtcdn.com/soa/gb/item/6771730315905003520/16393/goods_thumb_220-v1/9859bd33f57c.jpg",
+      } , {
+        title: "Y68 SSmart Watch Men Smartwatch For WomenMen Watches Kids Children Fitness Bracelet Smart Watch For Women Smartwatch",
+        price: 12.87,
+        orignalprice:"$13.20",
+        productID: 4,
+        img: "https://gloimg.gbtcdn.com/soa/gb/item/6878026654363611136/16419/goods_thumb_220-v1/316534621fbd.jpg",
+      },{
+        title: "Global Version MI 12 Ultra 7.3 inch Smartphone Cell Phone Unlocked 2022 5G Mobile Phones Cheap Android M12 Telephone",
+        price:69.92,
+        orignalprice:"$69.99",
+        productID: 14,
+        img: "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16485/goods_thumb_220-v1/3dd91decfe1a.jpg",
+      },  
+       {
+        title: "New M4PRO 6.7 Inch Global Version 256GB/512GB 6800mAh 16MP+32MP Smartphone Fingerprint Face ID Deca Core Mobile Phones MTK6889",
+        price: 69.92,
+        orignalprice:"$69.99",
+        productID: 15,
+        img: "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16563/goods_thumb_220-v1/e254e052fe64.jpg",
+      },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },{
+        title: "Tablet G18 GPS 8000mAh Google Play 8GB 128GB WIFI 10.1Inch Android 12 Laptop 48MP Deca Core 5G Pad Global Version PC",
+        price: 278.00,
+        orignalprice:"$69.99",
+        productID: 1,
+        img:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16602/goods_thumb_220-v1/d89677b30774.jpg"
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+let append_data=()=>{
+  
+    let contanier=document.getElementById("psresultsdiv")
+    contanier.innerHTML=null
+    psnewarrivaldata.forEach(el => {
+
+        let div =document.createElement("div")
+        div.setAttribute("class","psdiv")
+        
+
+        let imgdiv=document.createElement("div")
+        imgdiv.setAttribute("class","psdivimg")
+
+        let img=document.createElement("img")
+        img.src=el.img
+
+
+        let pdiv=document.createElement("div")
+        pdiv.setAttribute("class","psdivtitle")
+
+
+        let ptitle=document.createElement("p")
+        ptitle.innerText=el.title
+        ptitle.style.cursor="pointer"
+         
+         
+
+
+        let pprice=document.createElement("p")
+        pprice.innerText="$"+el.price
+        pprice.style.color="red"
+        pprice.style.fontSize="20px"
+
+        // let porignalprice=document.createElement("p")
+        // porignalprice.innerText=el.orignalprice
+        // porignalprice.style.color="#aab1ad"
+
+        imgdiv.append(img)
+        pdiv.append(ptitle)
+        div.append(imgdiv,pdiv,pprice)
+
+        contanier.append(div)
+
+
+    });
+}
+
+append_data()
+
+
+
+
+ 
+
+
+
+
+
+
+let append_data_animation=()=>{
+  
+    let contanier=document.getElementById("psanimationdiv")
+    contanier.innerHTML=null
+    Arrayofanimataion.forEach(el => {
+
+        let div =document.createElement("div")
+        div.setAttribute("class","psdivanimation")
+        
+
+        let imgdiv=document.createElement("div")
+        imgdiv.setAttribute("class","psdivimganimation")
+
+        let img=document.createElement("img")
+        img.src=el.img
+
+
+        let pdiv=document.createElement("div")
+        pdiv.setAttribute("class","psdivtitleanimation")
+
+
+        let ptitle=document.createElement("p")
+        ptitle.innerText=el.title
+        ptitle.style.cursor="pointer"
+         
+         
+
+
+        let pprice=document.createElement("p")
+        pprice.innerText="$"+el.price
+        pprice.style.color="red"
+        pprice.style.fontSize="20px"
+        pprice.setAttribute("class","animationprice")
+
+        // let porignalprice=document.createElement("p")
+        // porignalprice.innerText=el.orignalprice
+        // porignalprice.style.color="#aab1ad"
+
+        imgdiv.append(img)
+        pdiv.append(ptitle)
+        div.append(pdiv,imgdiv,pprice)
+
+        contanier.append(div)
+
+
+    });
+}
+
+  append_data_animation()
